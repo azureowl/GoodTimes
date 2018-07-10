3ba6b776a50f623e7faf199f4fb910d8f9824899
Azureowl <maiaroche16@gmail.com>
2018-07-10 14:52:39 -0700

    Added function for planner

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
+function addToEventListOrCalendar () {
+    console.log('addToEventListOrCalendar ran!');
+}
+
 function main () {
     app.eventbrite();
     app.fourSquare();
     app.darksky();
     // app.directions();
+    addToEventListOrCalendar();
 }
 
 $(main);
