330bea2aadc7ec36a710373ff76a2146a0534747
Azureowl <maiaroche16@gmail.com>
2018-07-14 00:20:36 -0700

    Tested function to seed events

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -20,15 +20,43 @@ app.eventbrite = function () {
     }
 
     // Generate Eventbrite with event information
-    function generateEventbriteEvents() {
+    function generateEventbriteEvents(data) {
         // event, event.venue_id
         // to get venue address url: https://www.eventbriteapi.com/v3/venues/${data.events[0].venue_id}/
         console.log('generateEventbriteEvents ran!');
-        $('.results-btn-image').css({
-            "background-image": `url('${userSeedData.eventbrite[0].logo.original.url}')`
+        // console.log(userSeedData.eventbrite);
+        // .js-autho-results append
+        console.log(data.events, 'hii');
+        const events = data.events;
+        let columns = 0;
+
+        `<div class="col col-4 results-margin">
+            <div class="results-cell">
+                <button class="results-btn-image"><img src="" alt=""></button>
+                <p class="result-title">Test</p>
+            </div>
+        </div>`
+        // when columns hits 3, then re-create inner-row-mb div
+
+        const results = events.map(function (event, i) {
+
+            const image = event.logo === null ? "" : event.logo.original.url;
+            const title = event.name.text ? event.name.text : "No Title";
+
+            // image is null then do something
+            return `<div class="col col-4 results-margin"><div class="results-cell"><button class="results-btn-image"><img src="${image}" alt=""></button><p class="result-title">${title}</p></div></div>`
         });
-        $('.result-title').text(`${userSeedData.eventbrite[0].name.text}`);
-        appendEventbriteEvents();
+
+        let eventResults = results.join('');
+        $('.test1').append(eventResults);
+
+
+        // $('.results-btn-image').css({
+        //     "background-image": `url('${userSeedData.eventbrite[0].logo.original.url}')`
+        // });
+
+        // $('.result-title').text(`${userSeedData.eventbrite[0].name.text}`);
+        // appendEventbriteEvents();
     }
 
     function appendEventbriteEvents () {
@@ -41,23 +69,31 @@ app.eventbrite = function () {
         const settings = {
             url: 'https://www.eventbriteapi.com/v3/events/search/',
             data: {
-                ['location.address']: data.seed.city
+                ['location.address']: data.seed.city,
+                page: 1
             },
             beforeSend: function (xhr) {
                 xhr.setRequestHeader("Authorization", `Bearer ${oAuth.access_token}`);
             }
         };
         $.ajax(settings).done(function (data) {
-            userSeedData.eventbrite = [];
-            data.events.forEach(event => {
-                // push events to userSeedData object
-                // console.log(event);
-                userSeedData.eventbrite.push(event);
-            });
-            generateEventbriteEvents();
+            // userSeedData.eventbrite = [];
+            console.log(data);
+            // data.events.forEach(event => {
+            //     // push events to userSeedData object
+            //     // console.log(event);
+            //     userSeedData.eventbrite.push(event);
+            // });
+            generateEventbriteEvents(data);
         }).fail(function (e) {
             console.log(e.statusText, e.responseText, "Call failed!");
         });
+    }
+
+    // should be able to page
+    function executePagination () {
+        console.log('executePagination ran!');
+        // keep track of page number to ensure you do not go before 1 or after
     }
 
     // On page load, check if there is OAuth authentication token
@@ -66,7 +102,10 @@ app.eventbrite = function () {
         if (window.location.hash) {
             const hash = window.location.hash;
             oAuth.access_token = hash.split('=')[2];
+            // call function to generate eventbrite markup
         } else {
+            // make sure a please login into eventbrite is displayed in the eventbrite section
+            // see .js-autho-results in index.html
             $('#js-eventbrite-login').on('click', function () {
                 window.location.replace(`${server.authorizeEndpoint}response_type=${server.response_type}&client_id=${config.eventbrite.key}`);
             });
