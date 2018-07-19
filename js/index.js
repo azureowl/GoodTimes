0d53b827ccfa12e903beec083784b214a0506732
Azureowl <maiaroche16@gmail.com>
2018-07-18 23:32:25 -0700

    completed planner list styling

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ function createPlan () {
     const detail = $("#todo").val();
     const date = $("#planner-date").val();
     const time = $("#time").val();
-    const html = `<li><span class="detail">${detail}</span><div class="detail-date"><span class="cal-date">${date}</span>, <span class="time-date">${time}</span></div><button class="delete">Delete</button></li>`;
+    const html = `<li><span class="detail">${detail}</span><div class="detail-date"><span class="cal-date">${date}</span>, <span class="time-date">${time}</span></div><button class="update">Update</button><button class="delete">Delete</button></li>`;
     appendPlan(html);
 }
 
@@ -26,8 +26,14 @@ function appendPlan (html) {
     $('.plan-list').append(html);
 }
 
+// function updatePlan () {
+//     $('.planner-viewer').on('click', '.update', function (e) {
+//         console.log($(e.currentTarget).parent(), $(e.target));
+//     });
+// }
+
 function deletePlan () {
-    $('body').on('click', '.delete', function (e) {
+    $('.planner-viewer').on('click', '.delete', function (e) {
         e.preventDefault();
         const li = $(this).closest('li');
         li.remove();
@@ -48,6 +54,7 @@ function togglePlanner () {
 function mainPlanner () {
     addToPlanner();
     deletePlan();
+    // updatePlan();
     togglePlanner();
 }
 
