daef1184abd988b6160a4cfec50e80e276e8a396
Azureowl <maiaroche16@gmail.com>
2018-07-22 19:09:57 -0700

    Added time with meridiem

--- js/index.js
+++ js/index.js
@@ -12,7 +12,9 @@ function createPlan () {
     const detail = $("#todo").val();
     const date = $("#planner-date").val();
     const time = $("#time").val();
-    const html = `<li><span class="detail">${detail}</span><div class="detail-date"><span class="cal-date">${date}</span>, <span class="time-date">${time}</span></div><button class="update">Update</button><button class="delete">Delete</button></li>`;
+    const newTime = timeWithMeridiem(time);
+    const html = `<li><span class="detail">${detail}</span><div class="detail-date"><span class="cal-date">${date}</span>, <span class="time-date">${newTime}</span></div><button class="delete">Delete</button></li>`;
+
     appendPlan(html);
 }
 
@@ -26,6 +28,19 @@ function deletePlan () {
         const li = $(this).closest('li');
         li.remove();
     });
+}
+
+function timeWithMeridiem (time) {
+    const hour = Number(time.split(':')[0]);
+    if (hour >= 12 && hour <= 23) {
+        return `${time} PM`;
+    } else if (hour >= 0 && hour <= 11) {
+        if (hour === 0) {
+            return `12:${time.split(':')[1]} AM`
+        } else {
+            return `${time} AM`;
+        }
+    }
 }
 
 function togglePlanner () {
@@ -46,7 +61,7 @@ function mainPlanner () {
 }
 
 function main () {
-    app.eventsAPIs();
+    // app.eventsAPIs();
     app.darksky.getUserLocWeather();
     mainPlanner();
 }
