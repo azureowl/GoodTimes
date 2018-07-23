446c115eb7a9e1e020913aa238ea52c33e58d7fb
Azureowl <maiaroche16@gmail.com>
2018-07-22 20:48:13 -0700

    save list items to localStorage

--- js/index.js
+++ js/index.js
@@ -9,12 +9,47 @@ function addToPlanner () {
 }
 
 function createPlan () {
+    const id = getRandomId();
     const detail = $("#todo").val();
     const date = $("#planner-date").val();
     const time = $("#time").val();
     const newTime = timeWithMeridiem(time);
-    const html = `<li><span class="detail">${detail}</span><div class="detail-date"><span class="cal-date">${date}</span>, <span class="time-date">${newTime}</span></div><button class="delete">Delete</button></li>`;
+    const html = `<li id="${id}"><span class="detail">${detail}</span><div class="detail-date"><span class="cal-date">${date}</span>, <span class="time-date">${newTime}</span></div><button class="delete">Delete</button></li>`;
+    saveListItemToStorage(id, html);
     appendPlan(html);
+}
+
+function getRandomId () {
+    var alph = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var number = '012345678910';
+    var ln = 5;
+    var id = '';
+
+    for (let i = 0; i <= ln; i++) {
+        id += alph.charAt(Math.floor(Math.random() * alph.length));
+        id += number.charAt(Math.floor(Math.random() * number.length));
+    }
+
+    return id;
+}
+
+// if local Storage exist use the content to populate on page load
+function hasLocalStorage () {
+    if (window.localStorage.length > 0) {
+        const localStorageObj = window.localStorage;
+        const html = Object.keys(localStorageObj).map(key => {
+            return localStorageObj[key];
+        });
+        $('.plan-list').append(html.join(''));
+    }
+}
+
+function saveListItemToStorage (id, html) {
+    localStorage.setItem(id, html);
+}
+
+function deleteListItemFromStorage (id) {
+    localStorage.removeItem(id);
 }
 
 function appendPlan (html) {
@@ -26,7 +61,9 @@ function deletePlan () {
     $('.planner-viewer').on('click', '.delete', function (e) {
         e.preventDefault();
         const li = $(this).closest('li');
+        const identifier = li.attr('id');
         li.remove();
+        deleteListItemFromStorage(identifier);
         toggleList();
     });
 }
@@ -54,6 +91,7 @@ function toggleList () {
 }
 
 function mainPlanner () {
+    hasLocalStorage();
     addToPlanner();
     deletePlan();
     toggleList();
